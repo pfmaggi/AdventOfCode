b6ac1f149acd4c474a725648f428cbc0b8d96480
Pietro F. Maggi <pfm@pietromaggi.com>
2021-12-21 21:11:33 +0100

    2021 - Day01-Day20 # Kotlin

---
+++ 2021/kotlin/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    application
+    kotlin("jvm") version "1.6.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    sourceSets {
+        main {
+            java.srcDirs("src/main/kotlin")
+        }
+        test {
+            java.srcDirs("src/test/kotlin")
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    wrapper {
+        gradleVersion = "7.3"
+    }
+}
+
+application {
+    mainClass.set("com.pietromaggi.aoc2021.day20.Day20Kt")
+}
