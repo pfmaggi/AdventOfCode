49b65a4b39096505e1ba98eebd71ffe5a51523cf
Pietro F. Maggi <pfm@pietromaggi.com>
2021-12-21 19:28:47 +0100

    2015 - Day02 part 2 in C.

--- 2015/c/src/day01.c
+++ 2015/c/src/day01.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Please enter filename to input data:\n");
+        printf("> %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     // Open the file
-    FILE *fp = fopen("../input/day01.txt", "r");
+    FILE *fp = fopen(argv[1], "r");
     if (!fp) {
         perror("File opening failed");
         return EXIT_FAILURE;
@@ -20,6 +26,7 @@ int main(void) {
     }
     fclose(fp);
 
+    printf("AoC2015 - Day01\n===============\n");
     printf("Final floor is: %ld\n", count);
     printf("Entered the basement at position: %ld\n", position);
 
