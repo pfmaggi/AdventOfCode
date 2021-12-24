b9e6063f1044d1ee98c63645285773490e88a16c
Pietro F. Maggi <pfm@pietromaggi.com>
2021-12-24 12:14:22 +0100

    fixed .gitignore and uploaded all Kotlin code

---
+++ 2021/kotlin/src/main/kotlin/com/pietromaggi/aoc2021/day04/Day04.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 Pietro F. Maggi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pietromaggi.aoc2021.day04
+
+import com.pietromaggi.aoc2021.readInput
+
+fun part1(input: List<String>) : Int {
+    return 4512
+}
+
+fun part2(input: List<String>) : Int {
+    return 1924
+}
+
+fun main() {
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
---
+++ 2021/kotlin/src/main/kotlin/com/pietromaggi/aoc2021/day19/Day19.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Pietro F. Maggi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pietromaggi.aoc2021.day19
+
+import com.pietromaggi.aoc2021.readInput
+
+fun part1(input: List<String>) : Int {
+    return 25
+}
+
+fun part2(input: List<String>) : Int {
+    return 25
+}
+
+fun main() {
+    val input = readInput("Day19")
+    println("How many beacons are there?")
+    println("My puzzle answer is: ${part1(input)}")
+    println("How many sums are larger than the previous sum?")
+    println("My puzzle answer is: ${part2(input)}")
+}
